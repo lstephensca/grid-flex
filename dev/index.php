24253dff103568b2e26659c27c65bd884c422eec
Unknown <github@chumneyads.com>
2018-08-22 10:11:26 -0400

    Initial set up of flexbox

---
+++ dev/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<head>
+	<title>New CSS Grid</title>
+	<!--<link rel="stylesheet" href="https://the-chumney.github.io/grid/css/compiled/cagrid.css">-->
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
+    <link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
+<link rel="stylesheet" href="css/dev.css">
+
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="column column-bottom">
+            <p>Microdosing unicorn biodiesel aesthetic. Ennui narwhal celiac live-edge four loko put a bird on it. PBR&B shabby chic trust fund adaptogen, lo-fi poutine butcher. Crucifix ennui vice viral woke truffaut. 3 wolf moon gluten-free letterpress, typewriter brunch pickled lo-fi hexagon adaptogen. Green juice retro poutine master cleanse humblebrag. Pok pok tumblr schlitz, VHS bicycle rights subway tile lo-fi try-hard snackwave tacos fanny pack small batch.</p>
+        </div>
+
+        <div class="column">
+            <p>Microdosing unicorn biodiesel aesthetic. Ennui narwhal celiac live-edge four loko put a bird on it. PBR&B shabby chic trust fund adaptogen, lo-fi poutine butcher. Crucifix ennui vice viral woke truffaut. 3 wolf moon gluten-free letterpress, typewriter brunch pickled lo-fi hexagon adaptogen. Green juice retro poutine master cleanse humblebrag. Pok pok tumblr schlitz, VHS bicycle rights subway tile lo-fi try-hard snackwave tacos fanny pack small batch.</p>
+            <p>Microdosing unicorn biodiesel aesthetic. Ennui narwhal celiac live-edge four loko put a bird on it. PBR&B shabby chic trust fund adaptogen, lo-fi poutine butcher. Crucifix ennui vice viral woke truffaut. 3 wolf moon gluten-free letterpress, typewriter brunch pickled lo-fi hexagon adaptogen. Green juice retro poutine master cleanse humblebrag. Pok pok tumblr schlitz, VHS bicycle rights subway tile lo-fi try-hard snackwave tacos fanny pack small batch.</p>
+            <p>Microdosing unicorn biodiesel aesthetic. Ennui narwhal celiac live-edge four loko put a bird on it. PBR&B shabby chic trust fund adaptogen, lo-fi poutine butcher. Crucifix ennui vice viral woke truffaut. 3 wolf moon gluten-free letterpress, typewriter brunch pickled lo-fi hexagon adaptogen. Green juice retro poutine master cleanse humblebrag. Pok pok tumblr schlitz, VHS bicycle rights subway tile lo-fi try-hard snackwave tacos fanny pack small batch.</p>
+            <p>Microdosing unicorn biodiesel aesthetic. Ennui narwhal celiac live-edge four loko put a bird on it. PBR&B shabby chic trust fund adaptogen, lo-fi poutine butcher. Crucifix ennui vice viral woke truffaut. 3 wolf moon gluten-free letterpress, typewriter brunch pickled lo-fi hexagon adaptogen. Green juice retro poutine master cleanse humblebrag. Pok pok tumblr schlitz, VHS bicycle rights subway tile lo-fi try-hard snackwave tacos fanny pack small batch.</p>
+            <p>Microdosing unicorn biodiesel aesthetic. Ennui narwhal celiac live-edge four loko put a bird on it. PBR&B shabby chic trust fund adaptogen, lo-fi poutine butcher. Crucifix ennui vice viral woke truffaut. 3 wolf moon gluten-free letterpress, typewriter brunch pickled lo-fi hexagon adaptogen. Green juice retro poutine master cleanse humblebrag. Pok pok tumblr schlitz, VHS bicycle rights subway tile lo-fi try-hard snackwave tacos fanny pack small batch.</p>
+        </div>
+
+
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+</div>
+
+</body>
+	<script src="https://www.theautohost.com/_assets/coding/js/jquery-2.1.4.js"></script>
+	<script defer src="https://www.theautohost.com/_assets/coding/fontawesome/js/fontawesome-all.js"></script>
+</html>
---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<head>
+	<title>New CSS Grid</title>
+	<!--<link rel="stylesheet" href="https://the-chumney.github.io/grid/css/compiled/cagrid.css">-->
+	<link rel="stylesheet" href="css/flexy.css">
+	<link rel="stylesheet" href="css/dev.css">
+
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="column">
+            <p>Fixie mustache street art, cardigan everyday carry readymade biodiesel mlkshk umami mixtape thundercats. Ramps try-hard chambray, drinking vinegar listicle activated charcoal mustache sartorial pour-over helvetica church-key enamel pin sustainable gluten-free four loko. Prism kitsch before they sold out gochujang iceland, cray post-ironic dreamcatcher kale chips artisan green juice intelligentsia salvia adaptogen PBR&B. Affogato lo-fi bushwick skateboard selvage leggings hot chicken tacos meggings iPhone.</p>
+        </div>
+        <div class="column">
+            <p>Fixie mustache street art, cardigan everyday carry readymade biodiesel mlkshk umami mixtape thundercats. Ramps try-hard chambray, drinking vinegar listicle activated charcoal mustache sartorial pour-over helvetica church-key enamel pin sustainable gluten-free four loko. Prism kitsch before they sold out gochujang iceland, cray post-ironic dreamcatcher kale chips artisan green juice intelligentsia salvia adaptogen PBR&B. Affogato lo-fi bushwick skateboard selvage leggings hot chicken tacos meggings iPhone.</p>
+        </div>
+
+        <div class="column">
+            <p>Fixie mustache street art, cardigan everyday carry readymade biodiesel mlkshk umami mixtape thundercats. Ramps try-hard chambray, drinking vinegar listicle activated charcoal mustache sartorial pour-over helvetica church-key enamel pin sustainable gluten-free four loko. Prism kitsch before they sold out gochujang iceland, cray post-ironic dreamcatcher kale chips artisan green juice intelligentsia salvia adaptogen PBR&B. Affogato lo-fi bushwick skateboard selvage leggings hot chicken tacos meggings iPhone.</p>
+        </div>
+
+  
+  <div class="column">
+            <p>Fixie mustache street art, cardigan everyday carry readymade biodiesel mlkshk umami mixtape thundercats. Ramps try-hard chambray, drinking vinegar listicle activated charcoal mustache sartorial pour-over helvetica church-key enamel pin sustainable gluten-free four loko. Prism kitsch before they sold out gochujang iceland, cray post-ironic dreamcatcher kale chips artisan green juice intelligentsia salvia adaptogen PBR&B. Affogato lo-fi bushwick skateboard selvage leggings hot chicken tacos meggings iPhone.</p>
+        </div>
+
+
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+	<script src="https://www.theautohost.com/_assets/coding/js/jquery-2.1.4.js"></script>
+	<script defer src="https://www.theautohost.com/_assets/coding/fontawesome/js/fontawesome-all.js"></script>
+</html>
